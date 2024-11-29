cf1ad4c88b0da71acfd979e3d23c13dcdb603eb3
catJam <loginerrore04@gmail.com>
2024-11-29 21:10:29 +0800

    🚮

--- Final/compile.ml
+++ Final/compile.ml
@@ -1,11 +1,21 @@
-
 open Format
 open X86_64
 open Ast
 
 let debug = ref false
 
-let file ?debug:(b=false) (p: Ast.tfile) : X86_64.program =
+type env =
+  { locals : (string * int) list (* Variable name and offset from %rbp *)
+  ; next_offset : int (* Next available offset (negative) *)
+  }
+
+(* let rec compile_texpr (expr : Ast.texpr) : X86_64.text = *)
+(* let rec compile_tstmt (stmt : Ast.tstmt) : X86_64.text = *)
+(* let compile_tdef ((fn, body) : Ast.tdef) : X86_64.text = *)
+(* let compile_tfile (tdefs : Ast.tfile) : X86_64.program = *)
+
+let file ?debug:(b = false) (p : Ast.tfile) : X86_64.program =
   debug := b;
-  { text = globl "main" ++ label "main" ++ ret;   (* TODO *)
-    data = nop; }                                 (* TODO *)
+  { text = globl "main" ++ label "main" ++ ret; (* TODO *)
+                                                data = nop }
+(* TODO *)
